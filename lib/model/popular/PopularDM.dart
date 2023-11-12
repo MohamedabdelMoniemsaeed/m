cda96238aa77f3eb7282db2bb637f3c0d8531973
MohamedabdelMoniemsaeed <mohamedalhawy458@gmail.com>
2023-11-12 19:30:53 +0200

    Add API HomeScreen

---
+++ lib/model/popular/PopularDM.dart
@@ -0,0 +1,120 @@
+
+class PopularDm {
+  PopularDm({
+      this.page, 
+      this.results, 
+      this.totalPages, 
+      this.totalResults,});
+
+  PopularDm.fromJson(dynamic json) {
+    page = json['page'];
+    if (json['results'] != null) {
+      results = [];
+      json['results'].forEach((v) {
+        results?.add(Results.fromJson(v));
+      });
+    }
+    totalPages = json['total_pages'];
+    totalResults = json['total_results'];
+  }
+  int? page;
+  List<Results>? results;
+  int? totalPages;
+  int? totalResults;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['page'] = page;
+    if (results != null) {
+      map['results'] = results?.map((v) => v.toJson()).toList();
+    }
+    map['total_pages'] = totalPages;
+    map['total_results'] = totalResults;
+    return map;
+  }
+
+}
+
+/// adult : false
+/// backdrop_path : "/628Dep6AxEtDxjZoGP78TsOxYbK.jpg"
+/// genre_ids : [28,53]
+/// id : 575264
+/// original_language : "en"
+/// original_title : "Mission: Impossible - Dead Reckoning Part One"
+/// overview : "Ethan Hunt and his IMF team embark on their most dangerous mission yet: To track down a terrifying new weapon that threatens all of humanity before it falls into the wrong hands. With control of the future and the world's fate at stake and dark forces from Ethan's past closing in, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan must consider that nothing can matter more than his mission—not even the lives of those he cares about most."
+/// popularity : 3180.725
+/// poster_path : "/NNxYkU70HPurnNCSiCjYAmacwm.jpg"
+/// release_date : "2023-07-08"
+/// title : "Mission: Impossible - Dead Reckoning Part One"
+/// video : false
+/// vote_average : 7.669
+/// vote_count : 2180
+
+class Results {
+  Results({
+      this.adult, 
+      this.backdropPath, 
+      this.genreIds, 
+      this.id, 
+      this.originalLanguage, 
+      this.originalTitle, 
+      this.overview, 
+      this.popularity, 
+      this.posterPath, 
+      this.releaseDate, 
+      this.title, 
+      this.video, 
+      this.voteAverage, 
+      this.voteCount,});
+
+  Results.fromJson(dynamic json) {
+    adult = json['adult'];
+    backdropPath = json['backdrop_path'];
+    genreIds = json['genre_ids'] != null ? json['genre_ids'].cast<int>() : [];
+    id = json['id'];
+    originalLanguage = json['original_language'];
+    originalTitle = json['original_title'];
+    overview = json['overview'];
+    popularity = json['popularity'];
+    posterPath = json['poster_path'];
+    releaseDate = json['release_date'];
+    title = json['title'];
+    video = json['video'];
+    voteAverage = json['vote_average'];
+    voteCount = json['vote_count'];
+  }
+  bool? adult;
+  String? backdropPath;
+  List<int>? genreIds;
+  int? id;
+  String? originalLanguage;
+  String? originalTitle;
+  String? overview;
+  double? popularity;
+  String? posterPath;
+  String? releaseDate;
+  String? title;
+  bool? video;
+  double? voteAverage;
+  int? voteCount;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['adult'] = adult;
+    map['backdrop_path'] = backdropPath;
+    map['genre_ids'] = genreIds;
+    map['id'] = id;
+    map['original_language'] = originalLanguage;
+    map['original_title'] = originalTitle;
+    map['overview'] = overview;
+    map['popularity'] = popularity;
+    map['poster_path'] = posterPath;
+    map['release_date'] = releaseDate;
+    map['title'] = title;
+    map['video'] = video;
+    map['vote_average'] = voteAverage;
+    map['vote_count'] = voteCount;
+    return map;
+  }
+
+}
